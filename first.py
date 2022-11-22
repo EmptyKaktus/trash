d4ad86c4569ef3ca2383733a974c023996e64dbb
EmptyKaktus <gabdrafikovs7@gmail.com>
2022-11-22 08:28:36 +0300

    4

--- first.py
+++ first.py
@@ -1,17 +1,22 @@
 import sys
 from random import randrange
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
 
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.btn.clicked.connect(self.ell_click)
         self.ell_TF = False
+        self.setGeometry(100, 100, 700, 700)
+        self.setWindowTitle('gfhdjsk')
+
+        self.btn = QPushButton(self)
+        self.btn.move(290, 320)
+        self.btn.resize(120, 30)
+        self.btn.setText('Draw ellipse')
+        self.btn.clicked.connect(self.ell_click)
 
     def paintEvent(self, event):
         if self.ell_TF:
